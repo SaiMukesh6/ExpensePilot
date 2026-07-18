df447a462a998d6ba1f3ae6f7b40901aa4d14df3
SaiMukesh6 <madabattulamukesh@gmail.com>
2026-07-18 12:16:14 +0530

    Prepare frontend for Vercel deployment

--- server/app.js
+++ server/app.js
@@ -7,7 +7,38 @@ const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 const app = express();
 
 // Middlewares
-app.use(cors());
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'http://127.0.0.1:5173',
+  'http://127.0.0.1:3000'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps, curl, etc.)
+    if (!origin) return callback(null, true);
+    
+    const clientUrl = process.env.CLIENT_URL;
+    const origins = [...allowedOrigins];
+    if (clientUrl) {
+      origins.push(clientUrl);
+    }
+    
+    const isAllowed = origins.some(allowedOrigin => {
+      const normOrigin = origin.replace(/\/$/, "");
+      const normAllowed = allowedOrigin.replace(/\/$/, "");
+      return normOrigin === normAllowed;
+    });
+
+    if (isAllowed) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 // Health Check Route
